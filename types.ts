e26613f227124b9fe6af023289b257eb5759deb3
Julia Fong <Juliamzfong@gmail.com>
2020-06-28 22:36:41 -0700

    First commit

---
+++ types.ts
@@ -0,0 +1,28 @@
+const today = new Date()
+
+
+//types shorcut to say here are the different
+//propterties and methods that this value right 
+//here has
+
+today.getMonth();
+
+const person = {
+    age: 20 
+}
+
+person.age
+
+class Color {
+    purple = ""
+}
+
+const red = new Color()
+red.purple
+
+//in TypeScript, any value has a type
+
+//Type Annotations + Type Inference
+//Variables
+//Functions
+//Objects
