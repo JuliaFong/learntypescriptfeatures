dbd6c731bd7039c57cc91b361b878071837f1fd8
Julia Fong <Juliamzfong@gmail.com>
2020-06-29 17:40:05 -0700

    Update notes

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -24,8 +24,64 @@ let apples: number = 5;
  let car: Car = new Car()
 
  //Object literal
- let point: { x: number; y: number } = {
+ let point: { x: number; y: number } = {  
      x: 10,
      y: 20
  }
+
+ //Function
+ const logNumber:(i: number) => void  = (i: number) => {
+    console.log(i)
+ }
+
+
+ //Type inference TypeScript guesses the type
+
+ //Type annontations developers tell Typescript the type
+
+
+//When to use annotations
+// 1) Function that returns the 'any' type 
+
+
+const json = "{'x': 10, 'y':20}"
+const coordinates: { x: number; y: number }  = JSON.parse(json)
+// add type annotation for coordinates 
+console.log(coordinates) // {x:10, y:20}
+
+// 2) when we declare a variable on one line
+// and initalizate it later
+
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord = true;
+    }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+
+
+let numbers = [-10, -1, 12]
+let numberAboveZero: boolean | number = false
+// the "pipe" |  is an OR statement 
+//numberAboveZero is a boolean OR a number
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i]
+    }
+}
+
+
+
+
+
+
+
+ 
+
+
 
