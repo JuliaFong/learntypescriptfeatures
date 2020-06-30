85713a462dacf4b2b585761d83d77475436f00af
Julia Fong <Juliamzfong@gmail.com>
2020-06-29 20:18:01 -0700

    Add annotations for anonymous functions

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,18 @@
 const add = (a: number, b: number): number => {
     return a + b
 };
+
+// a and b are both the arguments
+// number is the output
+
+const subtract = (a: number, b: number): number => {
+    return a - b
+}
+
+function divide(a: number, b: number): number {
+return a / b
+}
+
+const multiply = function(a: number, b: number): number  {
+    return a * b
+}
