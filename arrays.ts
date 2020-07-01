3c7119c60a00b8e1b2ba8f5edb1e1ab5846eee5e
Julia Fong <Juliamzfong@gmail.com>
2020-06-30 18:15:15 -0700

    Update array file

--- arrays.ts
+++ arrays.ts
@@ -2,4 +2,15 @@
 // In Typescript you're essentially working with plain JS
 // Arrays where each element is some consistent type of value
 
- 
+const carMakers: string[] = ['ford', 'toyota', 'chevy']
+const dates = [new Date(), new Date()]
+
+const carsByMake = [
+    ['f150'],
+    ['corolla'],
+    ['camaro']
+];
+
+//OR
+ const carMakes: string[] = [];
+ const carsByMakers: string[][] = [];
