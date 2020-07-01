baadaa06179e4487af7a5cefd2dc0cfb5941a0fe
Julia Fong <Juliamzfong@gmail.com>
2020-06-30 17:52:33 -0700

    Add objects file

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -16,3 +16,39 @@ return a / b
 const multiply = function(a: number, b: number): number  {
     return a * b
 }
+
+const logger = (message: string): void => {
+    console.log(message)
+    return null
+    return undefined
+}
+
+const throwError = (message: string): void => {
+    if (!message) {
+        throw new Error(message);
+    }
+}
+
+const todaysWeather = {
+    date: new Date(),
+    weather: 'sunny'
+};
+
+const logWeather = ({date, weather}: {date: Date, weather: string}): void => {
+    console.log(date)
+    console.log(weather)
+};
+logWeather(todaysWeather);
+
+const loggingWeather = (forecast: {date: Date, weather: string}): void => {
+    console.log(forecast.date)
+    console.log(forecast.weather)
+};
+loggingWeather(todaysWeather)
+
+// ES2015 syntax
+// const logWeather = ({date, weather}) => {
+//     console.log(date)
+//     console.log(weather)
+// }
+//logWeather(todaysWeather)
