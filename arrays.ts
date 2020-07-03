a1567710833556f3333ff3099e77976832f7b5f8
Julia Fong <Juliamzfong@gmail.com>
2020-07-02 19:31:50 -0500

    Add array note

--- arrays.ts
+++ arrays.ts
@@ -14,3 +14,15 @@ const carsByMake = [
 //OR
  const carMakes: string[] = [];
  const carsByMakers: string[][] = [];
+
+ //Typed Arrays
+
+ //Arrays where each element is some consistent type of value
+
+ //TS can do type inferenec when extracting values from an array
+
+ //TS can prevent us from adding compatible values to the array
+
+ //We can get help with 'map', 'forEach', 'reduce' functions
+
+ //Flexible - arrays can still contain multiple different types
