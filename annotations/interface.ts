9fe7dd44d9c9f638577085a19f56ea597e069a2c
Julia Fong <Juliamzfong@gmail.com>
2020-07-05 18:25:48 -0500

    Add long type annotation example

--- annotations/interface.ts
+++ annotations/interface.ts
@@ -2,3 +2,17 @@
 // Creates a new type describing the property names
 // and value types of an object
 // A custom type the developer will design
+
+const oldCivic = {
+    name: 'civic',
+    year: 2000,
+    broken: true
+};
+
+const printVehicle = (vehicle: { name: string, year: number, broken: boolean }): void => {
+    console.log(`Name: ${vehicle.name}`)
+    console.log(`Year: ${vehicle.year}`)
+    console.log(`Broken:  ${vehicle.broken}`)
+}
+
+printVehicle(oldCivic)
