8d2e3dbbbbec1951ae0ecf66f7e27e46e9108ebb
Julia Fong <Juliamzfong@gmail.com>
2020-07-07 12:42:39 -0500

    Update classes notes

--- annotations/classes.ts
+++ annotations/classes.ts
@@ -2,3 +2,27 @@
 
 // Blueprint to create and object with some fields
 // (values) and methods (functions) to represent a 'thing
+
+// Classes FIRST
+
+// define a set of fields (values) and methods (functions)
+//to represent a 'thing'
+
+//SECONDLY they define a set of fields (values) and
+// methodes (functions) to represent a 'thing'
+
+// ALWAYS capitalize a name of a class
+
+class Vehicle {
+    drive(): void {
+        console.log('vroom vroom')
+    }
+
+    honk(): void {
+        console.log('beep')
+    }
+}
+
+const vehicle = new Vehicle () 
+vehicle.drive()
+vehicle.honk()
