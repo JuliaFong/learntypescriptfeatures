a5dff4058a9cae394d98c47bb032edfafd2173dd
Julia Fong <Juliamzfong@gmail.com>
2020-07-07 14:05:13 -0500

    Add section eight

--- annotations/classes.ts
+++ annotations/classes.ts
@@ -14,15 +14,69 @@
 // ALWAYS capitalize a name of a class
 
 class Vehicle {
-    drive(): void {
+    constructor(public color: string) {
+    }
+    public drive(): void {
         console.log('vroom vroom')
     }
 
-    honk(): void {
+    public name() {
+        
+    } protected honk(): void {
         console.log('beep')
     }
 }
 
-const vehicle = new Vehicle () 
-vehicle.drive()
-vehicle.honk()
+const vehicle = new Vehicle('orange')
+console.log(vehicle.color)
+// vehicle.honk()
+
+class Car extends Vehicle {
+    constructor(public wheels: number, color: string){
+        super('color')
+    }
+
+    private drive(): void {
+        console.log('vroom')
+    }
+
+    startDrivingProcess(): void {
+        this.drive()
+        this.honk()
+    }
+}
+
+const car = new Car(4, 'color')
+car.startDrivingProcess()
+// car.drive() // can't call method outside of class
+// car.honk()
+
+// const vehicle = new Vehicle ('purple') 
+// vehicle.drive()
+// vehicle.honk()
+
+// **************************** //
+
+// MODIFIERS
+
+// can be public, private, and protected
+
+// public method can be called any where, anytime
+
+// private method can only be called by other methods
+// in this class
+
+// protected method can be called by other 
+//methods in this class, or by other methods
+//in child classes
+
+// ***************
+
+//FIELDS in classes & inheritance
+
+//where to use classes 
+
+// Interfaces + classes 
+
+// is how we get really strong code reuse in TS
+
